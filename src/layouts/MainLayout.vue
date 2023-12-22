783531532be7a8d6100865285b90a1695aeb2c26
teddy <teddy.jeon@speedwagon.io>
2023-12-22 12:33:05 +0900

    setup main frame: header/sidebar/navbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,62 +1,97 @@
 <template>
   <q-layout view="lHh lpR lFf">
-
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn v-if="!isMobile" dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-btn
+          v-if="!isMobile"
+          dense
+          flat
+          round
+          icon="menu"
+          @click="toggleLeftDrawer"
+        />
 
         <q-toolbar-title>
-          <img src="../assets/speedwagon_logo.png" class="logo">
+          <img src="../assets/speedwagon_logo.png" class="logo" />
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-if="!isMobile" v-model="leftDrawerOpen" side="left" overlay behavior="mobile" bordered>
-      <!-- drawer content -->
+    <q-drawer
+      v-if="!isMobile"
+      v-model="leftDrawerOpen"
+      side="left"
+      overlay
+      behavior="mobile"
+      bordered
+    >
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
 
-    <q-footer v-if="isMobile" class="bg-grey-8 text-white">
+    <q-footer v-if="isMobile" class="bg-grey-4 text-white">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
-            <img src="../assets/speedwagon_logo.png">
+            <img src="../assets/speedwagon_logo.png" />
           </q-avatar>
         </q-toolbar-title>
       </q-toolbar>
     </q-footer>
-
   </q-layout>
 </template>
 
 <script>
-import { computed, ref } from 'vue'
+import { defineComponent, computed, ref } from 'vue'
 import { useQuasar } from 'quasar'
 
-export default {
-  setup () {
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev',
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
     const leftDrawerOpen = ref(false)
 
     const quasar = useQuasar()
-
     const isMobile = computed(() => {
-      return quasar.screen.lt.md
-        ? true
-        : false
+      return quasar.screen.lt.md ? true : false
     })
 
     return {
       leftDrawerOpen,
-      toggleLeftDrawer () {
+      essentialLinks: linksList,
+      toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value
       },
-      isMobile
+      isMobile,
     }
-  }
-}
+  },
+})
 </script>
 
 <style lang="scss">
