cb0543d286122e259edb11a602f0543fc7bfbec5
teddy <teddy.jeon@speedwagon.io>
2023-12-29 00:06:44 +0900

    vue/multi-word-component-names off

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -85,6 +85,8 @@ module.exports = {
     'no-unused-vars': 'off',
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    'vue/multi-word-component-names': 'off'
   }
 }
