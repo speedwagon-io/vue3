1859fd42949f8fdbf72cd33e5a3c2967d3880adf
teddy <teddy.jeon@speedwagon.io>
2024-01-11 13:48:16 +0900

    amplifyconfig modified

--- amplifyconfig.ts
+++ amplifyconfig.ts
@@ -1,21 +1,21 @@
 import type { ResourcesConfig, AuthUserPoolConfig } from '@aws-amplify/core'
 
 // const DomainName = 'd-speedwagon'
-const DomainName = 'kakaoauthdemodev821343553435'
+const DomainName = 'kakaoauthtest'
 
 const AuthConfig: AuthUserPoolConfig = {
   Cognito: {
     // userPoolId: 'ap-northeast-2_6voU3H3y5',
-    userPoolId: 'ap-northeast-2_nk8Ccq37i',
+    userPoolId: 'ap-northeast-2_Ql6fMPkzw',
     // userPoolClientId: '3iraugrtsm6qo3pifn0lron2rq',
-    userPoolClientId: '49ogj0sn3c47gj14caoeo0lr4j',
+    userPoolClientId: '64n23mmpdnc7qh6rghrqef9l4d',
     loginWith: {
       oauth: {
         domain: `${DomainName}.auth.ap-northeast-2.amazoncognito.com`,
         scopes: ['openid'],
-        redirectSignIn: [`${process.env.APP_URL}/auth/kakao/callback`],
-        redirectSignOut: [process.env.APP_URL || 'http://localhost:9000'],
-        responseType: 'token',
+        redirectSignIn: [`${process.env.APP_URL_NO_TRAILING_SLASH}/auth/kakao/callback`],
+        redirectSignOut: [process.env.APP_URL_NO_TRAILING_SLASH || 'http://localhost:9000'],
+        responseType: 'code',
       },
       username: true,
     },
