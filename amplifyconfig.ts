6b6cc83e0525062ee105c205b8d2aeb6fb108810
teddy <teddy.jeon@speedwagon.io>
2024-01-11 16:22:05 +0900

    env variables for amplifyconfig

--- amplifyconfig.ts
+++ amplifyconfig.ts
@@ -1,17 +1,12 @@
 import type { ResourcesConfig, AuthUserPoolConfig } from '@aws-amplify/core'
 
-// const DomainName = 'd-speedwagon'
-const DomainName = 'kakaoauthtest'
-
 const AuthConfig: AuthUserPoolConfig = {
   Cognito: {
-    // userPoolId: 'ap-northeast-2_6voU3H3y5',
-    userPoolId: 'ap-northeast-2_Ql6fMPkzw',
-    // userPoolClientId: '3iraugrtsm6qo3pifn0lron2rq',
-    userPoolClientId: '64n23mmpdnc7qh6rghrqef9l4d',
+    userPoolId: process.env.COGNITO_USERPOOL_ID || '',
+    userPoolClientId: process.env.COGNITO_USERPOOL_CLIENT_ID || '',
     loginWith: {
       oauth: {
-        domain: `${DomainName}.auth.ap-northeast-2.amazoncognito.com`,
+        domain: `${process.env.COGNITO_DOMAIN}.auth.ap-northeast-2.amazoncognito.com`,
         scopes: ['openid'],
         redirectSignIn: [`${process.env.APP_URL_NO_TRAILING_SLASH}/auth/kakao/callback`],
         redirectSignOut: [process.env.APP_URL_NO_TRAILING_SLASH || 'http://localhost:9000'],
