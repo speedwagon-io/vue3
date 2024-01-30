aee40498cc22722bd3bd0b81ecaeb6fcf6ddace3
teddy <teddy.jeon@speedwagon.io>
2024-01-30 16:02:43 +0900

    yarn format

--- src/components/input/QnAModeToggle.vue
+++ src/components/input/QnAModeToggle.vue
@@ -1,27 +1,27 @@
 <template>
-	<q-btn-toggle
-		v-model="mode"
-		class="q-pa-sm"
-		no-caps
-		rounded
-		unelevated
-		toggle-color="primary"
-		color="grey-2"
-		text-color="grey-5"
-		:options="[
-			{label: '질문자모드', value: 'query'},
-			{label: '답변자모드', value: 'wagon'}
-		]"
-	/>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, ref } from 'vue'
-  
-  export default defineComponent({
-    name: 'QnAModeToggle',
-    setup() {
-      return { mode: ref('query') }
-    },
-  })
-  </script>
+  <q-btn-toggle
+    v-model="mode"
+    class="q-pa-sm"
+    no-caps
+    rounded
+    unelevated
+    toggle-color="primary"
+    color="grey-2"
+    text-color="grey-5"
+    :options="[
+      { label: '질문자모드', value: 'query' },
+      { label: '답변자모드', value: 'wagon' },
+    ]"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'QnAModeToggle',
+  setup() {
+    return { mode: ref('query') }
+  },
+})
+</script>
