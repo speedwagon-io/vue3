d0e410452e11dd2e286fdb716064a3d75fae5155
teddy <teddy.jeon@speedwagon.io>
2024-01-30 16:01:47 +0900

    css

--- src/components/input/QnAModeToggle.vue
+++ src/components/input/QnAModeToggle.vue
@@ -6,8 +6,8 @@
 		rounded
 		unelevated
 		toggle-color="primary"
-		color="white"
-		text-color="primary"
+		color="grey-2"
+		text-color="grey-5"
 		:options="[
 			{label: '질문자모드', value: 'query'},
 			{label: '답변자모드', value: 'wagon'}
