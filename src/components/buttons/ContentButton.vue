c8e90f0075642aafcda5815ae9b64bdeb5cb76e1
teddy <teddy.jeon@speedwagon.io>
2024-02-06 10:17:10 +0900

    ContentButton added

---
+++ src/components/buttons/ContentButton.vue
@@ -0,0 +1,33 @@
+<template>
+<q-btn>
+	<div class="row justify-between items-center full-width">
+		<div class="column items-start">
+			<div>0분전 0답변</div>
+			<span>{{ title }}</span>
+			<span>{{ content }}</span>
+		</div>
+		<q-icon name="chevron_right" />
+	</div>
+</q-btn>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'ContentButton',
+	props: {
+		title: {
+			type: String,
+			required: true
+		},
+		content: {
+			type: String,
+			required: true
+		},
+	},
+	setup() {
+		return {}
+	}
+})
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,7 @@
   <q-page padding>
     <section>
       <p>진행중인 질문(0)</p>
+      <ContentButton :title="'제목'" :content="'내용'" />
     </section>
 
     <section>
@@ -13,7 +14,16 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import ContentButton from 'components/buttons/ContentButton.vue'
+
 export default defineComponent({
   name: 'Home',
+  components: { ContentButton }
 })
 </script>
+
+<style lang="scss" scoped>
+section {
+  padding-bottom: 20px;
+}
+</style>
