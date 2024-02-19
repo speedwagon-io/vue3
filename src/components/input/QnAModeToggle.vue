0e4c1b9262a78d2008ed04f0f4f813f99645c648
Taeho Jeon <teddy.jeon@speedwagon.io>
2024-02-19 22:37:48 +0900

    QnAModeToggle pre-done with TODOs

--- src/components/input/QnAModeToggle.vue
+++ src/components/input/QnAModeToggle.vue
@@ -19,26 +19,43 @@
 import { defineComponent, onMounted, ref } from 'vue'
 
 import { useUserSession } from 'src/composition/useUserSession'
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'QnAModeToggle',
   setup() {
     const router = useRouter()
-    // const { isAuthenticated } = useUserSession()
+    const route = useRoute()
+    const { isAuthenticated } = useUserSession()
 
     const mode = ref('query')
 
-    onMounted(() => {
+    onMounted(async () => {
       const userMode = localStorage.getItem('userMode')
+      if (!(await isAuthenticated(null)) && userMode === 'answer') {
+        mode.value = 'query'
+        return
+      }
       mode.value = userMode || 'query'
     })
 
     const handleModeClicked = async () => {
       localStorage.setItem('userMode', mode.value)
-      router.push('/').then(() => {
-        router.go(0)
-      })
+      // TODO] 홈으로 보내고 리프레시가 맞나?
+      if (mode.value === 'query') {
+        router.push('/').then(() => {
+          router.go(0)
+        })
+      } else {
+        // TODO] 답변자 온보딩
+        if (await isAuthenticated(route.fullPath)) {
+          router.push('/').then(() => {
+            router.go(0)
+          })
+        } else {
+          mode.value = 'query'
+        }
+      }
     }
 
     return { mode, handleModeClicked }
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,17 +5,21 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => {
-        const userMode = localStorage.getItem('userMode')
-        switch (userMode) {
-          case 'query':
-            return import('pages/QModeHome.vue')
-          case 'answer':
-            return import('pages/AModeHome.vue')
-          default:
-            return import('pages/QModeHome.vue')
-        }
-      }},
+      {
+        path: '',
+        name: 'home',
+        component: () => {
+          const userMode = localStorage.getItem('userMode')
+          switch (userMode) {
+            case 'query':
+              return import('pages/QModeHome.vue')
+            case 'answer':
+              return import('pages/AModeHome.vue')
+            default:
+              return import('pages/QModeHome.vue')
+          }
+        },
+      },
       { path: 'index', component: () => import('pages/IndexPage.vue') },
       { path: 'login', component: () => import('pages/auth/Login.vue') },
       {
